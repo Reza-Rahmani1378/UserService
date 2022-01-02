abf982cde6e12fc5d5f5ae42532158f51838c911
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-01-02 19:24:30 +0330

    Add Entity Admin

---
+++ ProgrammingChallenge/ProgrammingChallenge/src/main/java/ir/bontec/programmingchallenge/entities/Admin.java
@@ -0,0 +1,24 @@
+package ir.bontec.programmingchallenge.entities;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = Admin.TABLE_NAME)
+@NoArgsConstructor
+@Builder
+public class Admin extends User {
+
+    public static final String TABLE_NAME = "admin_user";
+
+}
+
+
