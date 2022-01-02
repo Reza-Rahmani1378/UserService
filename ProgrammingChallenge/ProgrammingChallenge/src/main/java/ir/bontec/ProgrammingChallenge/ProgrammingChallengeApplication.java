4efd2a1e9490f02f4a5b67153e465c4ef80e275e
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-01-02 17:28:05 +0330

    Files Needed For Project

---
+++ ProgrammingChallenge/ProgrammingChallenge/src/main/java/ir/bontec/ProgrammingChallenge/ProgrammingChallengeApplication.java
@@ -0,0 +1,13 @@
+package ir.bontec.ProgrammingChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammingChallengeApplication.class, args);
+    }
+
+}
---
+++ ProgrammingChallenge/ProgrammingChallenge/src/test/java/ir/bontec/ProgrammingChallenge/ProgrammingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.bontec.ProgrammingChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
